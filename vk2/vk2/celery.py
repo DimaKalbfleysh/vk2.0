b092335db36cbbe98b82c4a8b719855b057efd3a
Dima <kalbfleysg@gmail.com>
2019-03-17 13:13:31 +0700

    added check email using celery

--- vk2/vk2/celery.py
+++ vk2/vk2/celery.py
@@ -2,9 +2,10 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vk2.settings')
+os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
 
-app = Celery('vk2')
+app = Celery('vk2', broker='redis://127.0.0.1:6379/0')
 app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+
--- vk2/vk2/settings.py
+++ vk2/vk2/settings.py
@@ -138,8 +138,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "kalbf.rabota@gmail.com"
-EMAIL_HOST_PASSWORD = "kalbfleysh@gmail.com1"
+EMAIL_HOST_USER = "your_email"
+EMAIL_HOST_PASSWORD = "password"
 EMAIL_USE_TLS = True
 
 REDIS_HOST = 'localhost'
